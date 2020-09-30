ba508daec6d436c2e17a0f95754dc2cfba872a00
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-09-30 15:16:17 +0200

    Puppet agent conf

--- manifests/baseconfig.pp
+++ manifests/baseconfig.pp
@@ -2,6 +2,7 @@
 class profile::baseconfig {
   include ::profile::baseconfig::networking
   include ::profile::baseconfig::packages
+  include ::profile::baseconfig::puppet
   include ::profile::baseconfig::time
   include ::profile::baseconfig::users
 }
