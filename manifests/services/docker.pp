73c1cef80bc8b8314aaef916b4726c012be58a51
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-10-23 12:43:58 +0200

    Add DNS conf to docker daemon

--- manifests/services/docker.pp
+++ manifests/services/docker.pp
@@ -1,6 +1,13 @@
 # Install docker
 class profile::services::docker {
 
-  class { '::docker': }
+  $dns_servers = split(lookup('profile::dns::nameservers'), ' ')
+  $dns_search = lookup('profile::dns::searchdomain')
+
+  class { '::docker':
+    dns        => $dns_servers,
+    dns_search => $dns_search,
+  }
+
   class { '::docker::compose': }
 }
