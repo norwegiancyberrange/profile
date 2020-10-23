b1fd0f1e3d4dddbf32383c1b3311b4dec765661c
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-10-23 10:47:44 +0200

    rabbit profile

---
+++ manifests/services/rabbitmq.pp
@@ -0,0 +1,26 @@
+# Install rabbitmq server
+class profile::services::rabbitmq {
+
+  $password = lookup('profile::rabbitmq::password')
+
+  if ( $facts['networking']['scope6'] =~ /global/ ) {
+    $ipv6 = true
+  } else {
+    $ipv6 = false
+  }
+
+  class { '::rabbitmq':
+    repos_ensure      => true,
+    delete_guest_user => true,
+    ipv6              => $ipv6
+  }
+  -> rabbitmq_user { 'rabbit':
+    admin    => true,
+    password => $password,
+  }
+  -> rabbitmq_user_permissions { 'rabbit@/':
+    configure_permission => '.*',
+    write_permission     => '.*',
+    read_permission      => '.*',
+  }
+}
