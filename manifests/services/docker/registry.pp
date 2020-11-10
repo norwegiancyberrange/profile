59f6f091c862a8707fead7b303d0c12877508e3d
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-11-10 13:02:31 +0100

    Woops. too much undef

--- manifests/services/docker/registry.pp
+++ manifests/services/docker/registry.pp
@@ -1,9 +1,9 @@
 # Setup auth to our internal docker registry
 class profile::services::docker::registry {
 
-  $registry = lookup('profile::docker::registry::name', Variant[String,Boolean], undef, undef, false)
-  $username = lookup('profile::docker::registry::username', Variant[String,Boolean], undef, undef, false)
-  $password = lookup('profile::docker::registry::password', Variant[String,Boolean], undef, undef, false)
+  $registry = lookup('profile::docker::registry::name', Variant[String,Boolean], undef, false)
+  $username = lookup('profile::docker::registry::username', Variant[String,Boolean], undef, false)
+  $password = lookup('profile::docker::registry::password', Variant[String,Boolean], undef, false)
 
   if ($registry) {
     docker::registry { $registry:
