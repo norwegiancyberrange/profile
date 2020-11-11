eb305473b60c0789ef276612107b6b917779a886
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-11-11 10:03:11 +0100

    Wrong class names

--- manifests/services/docker/ncr.pp
+++ manifests/services/docker/ncr.pp
@@ -1,7 +1,7 @@
 # Define the NCR containers
 class profile::services::docker::ncr {
 
-  require ::profile::ncr::registry
-  include ::profile::ncr::containers
+  require ::profile::services::ncr::registry
+  include ::profile::services::ncr::containers
 
 }
