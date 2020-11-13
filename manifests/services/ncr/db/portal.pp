0ea6865c8f745ab07175e86c0f55e014555054e0
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-11-13 12:38:56 +0100

    Add pg_hba rule for portal-server

--- manifests/services/ncr/db/portal.pp
+++ manifests/services/ncr/db/portal.pp
@@ -2,6 +2,7 @@
 class profile::services::ncr::db::portal {
 
   $portal_password = lookup('profile::ncr::portal::db::password', String)
+  $portal_server   = lookup('profile::ncr::portal::ip')
 
   postgresql::server::db { 'portal':
     user     => 'portal',
@@ -9,4 +10,12 @@ class profile::services::ncr::db::portal {
     owner    => 'portal',
   }
 
+  postgresql::server::pg_hba_rule { 'allow portal access':
+    description => 'Allow the portal server to connect',
+    type        => 'host',
+    database    => 'portal',
+    user        => 'portal',
+    address     => "${portal_server}/32",
+    auth_method => 'md5',
+  }
 }
